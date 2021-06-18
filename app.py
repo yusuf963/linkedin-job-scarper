7f672bde5e80711e6de6bd266ec3f0da18d17317
yusuf963 <mohammad963yusuf@gmail.com>
2021-06-18 15:17:35 +0100

    install linkein-scraper-lib

---
+++ app.py
@@ -0,0 +1,6 @@
+import logging
+from linkedin_jobs_scraper import LinkedinScraper
+from linkedin_jobs_scraper.events import Events, EventData
+from linkedin_jobs_scraper.query import Query, QueryFilters, QueryOptions
+from linkedin_jobs_scraper.filters import RelevanceFilters, TimeFilters, TypeFilters, ExperienceLevelFilters, RemoteFilters
+
