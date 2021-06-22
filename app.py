378ef91cb8be50af50ab0eba8044fdde7a230eb8
yusuf963 <mohammad963yusuf@gmail.com>
2021-06-22 14:43:19 +0100

    create csv file with scraped data

--- app.py
+++ app.py
@@ -10,24 +10,30 @@ def on_data(data: EventData):
     print('[On_DATA]', data.title, data.company,
           data.date, data.link, len(data.description))
 
-def on_error(error):
-    print('[ON_ERROR]', error)
+
+# def on_error(error):
+#     print('[ON_ERROR]', error)
+
 
 def on_end():
     print('[On_END]')
 
+
 print('I am here')
 
 scraper = LinkedinScraper(
-    chrome_executable_path="\c:\Windows\chromedriver.exe",   # Custom Chrome executable path (e.g. /foo/bar/bin/chromedriver)
+    # Custom Chrome executable path (e.g. /foo/bar/bin/chromedriver)"\c:\Windows\chromedriver.exe"
+    chrome_executable_path=None,
     chrome_options=None,  # Custom Chrome options here
     headless=True,  # Overrides headless mode only if chrome_options is None
-    max_workers=1,    # How many threads will be spawned to run queries concurrently (one Chrome driver for each thread)
-    slow_mo=1.3,    # Slow down the scraper to avoid 'Too many requests (429)' errors
+    # How many threads will be spawned to run queries concurrently (one Chrome driver for each thread)
+    max_workers=1,
+    # Slow down the scraper to avoid 'Too many requests (429)' errors
+    slow_mo=2.3,
 )
 # Add event listeners
 scraper.on(Events.DATA, on_data)
-scraper.on(Events.ERROR, on_error)
+# scraper.on(Events.ERROR, on_error)
 scraper.on(Events.END, on_end)
 
 queries = [
@@ -44,7 +50,7 @@ queries = [
             optimize=False,
             limit=5,
             filters=QueryFilters(
-                company_jobs_url='https://www.linkedin.com/jobs/search/?f_C=1441%2C17876832%2C791962%2C2374003%2C18950635%2C16140%2C10440912&geoId=92000000',
+                company_jobs_url='https://www.linkedin.com/jobs/search/?f_C=162479&geoId=92000000',
                 relevance=RelevanceFilters.RECENT,
                 time=TimeFilters.MONTH,
                 type=[TypeFilters.FULL_TIME, TypeFilters.INTERNSHIP],
