8bccfc5543247f210defe14a9836ea3245ed79fc
elsahib <elsahib10@gmail.com>
2021-06-22 22:53:08 +0100

    produced a csv with ops start timestamp

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const searchKeyWord = document.getElementById("demo");
-console.log(searchKeyWord)
-alert('hello')
 
+
+const fs = require("fs");
+const stringify = require("csv-stringify")
+const moment = require('moment');
+timestamp = moment().format('DD-MM-YYYY-HH-m-s')
 const {
     LinkedinScraper,
     relevanceFilter,
@@ -10,17 +12,28 @@ const {
     experienceLevelFilter,
     events,
 } = require("linkedin-jobs-scraper");
+scraped_data = []
+let columns = {
+    title:'Job Title', 
+    desc :'Description', 
+    query:'Query', 
+    loc:'Job Location', 
+    jobId: 'Job Id', 
+    company:'Company Name', 
+    level:'Senority Level', 
+    func:'function', 
+    link:'Job Link',
+    type: 'Emplyment Type',
+    applyLink: 'Apply Link'
+    };
 
-const fs = require('fs')
-const writeStream = fs.createWriteStream('result.csv');
-// writeStream.write(`Description, Query, Location, Id, Title,Company, CompanyUrl, senorityLevel, function, Link \n`)
 
 (async () => {
     // Each scraper instance is associated with one browser.
     // Concurrent queries will run on different pages within the same browser instance.
     const scraper = new LinkedinScraper({
         headless: true,
-        slowMo: 900,
+        slowMo: 500,
         args: [
             "--lang=en-GB",
         ],
@@ -29,33 +42,53 @@ const writeStream = fs.createWriteStream('result.csv');
     // Add listeners for scraper events
     scraper.on(events.scraper.data, (data) => {
         console.log(
-            data.description.length,
+            // data.description.length,
             // data.descriptionHTML.length,
             // data.description,
-            `Description='${data.description.replace('\n', '')}'`,
-            `Query='${data.query}'`,
-            `Location='${data.location}'`,
-            `Id='${data.jobId}'`,
+            // `Description='${data.description.replace('\n', '')}'`,
+            // `Query='${data.query}'`,
+            // `Location='${data.location}'`,
+            // `Id='${data.jobId}'`,
             `Title='${data.title}'`,
-            `Company='${data.company ? data.company : "N/A"}'`,
-            `CompanyURL='${data.company.link ? data.company : "N/A"}'`,
-            `Place='${data.place}'`,
-            `Date='${data.date}'`,
-            `Link='${data.link}'`,
-            `applyLink='${data.applyLink ? data.applyLink : "N/A"}'`,
-            `senorityLevel='${data.senorityLevel}'`,
-            `function='${data.jobFunction}'`,
-            `employmentType='${data.employmentType}'`,
-            `industries='${data.industries}'`,
+            // `Company='${data.company ? data.company : "N/A"}'`,
+            // `CompanyURL='${data.company.link ? data.company : "N/A"}'`,
+            // `Place='${data.place}'`,
+            // `Date='${data.date}'`,
+            // `Link='${data.link}'`,
+            // `applyLink='${data.applyLink ? data.applyLink : "N/A"}'`,
+            // `senorityLevel='${data.senorityLevel}'`,
+            // `function='${data.jobFunction}'`,
+            // `employmentType='${data.employmentType}'`,
+            // `industries='${data.industries}'`,
         );
-        writeStream.write(`${data.description.replace('\n', '')}, ${data.query}, ${data.location}, ${data.jobId},${data.title}, ${data.company ? data.company : "N/A"}, ${data.senorityLevel}, ${data.jobFunction}, ${data.link} `)
+        scraped_data.push([
+            data.title,
+            data.description.replace(/[\s\n\r]+/g, " ").trim(),
+            data.query,
+            data.location,
+            data.jobId,
+            data.company,
+            data.senorityLevel,
+            data.jobFunction,
+            data.link,
+            data.employmentType,
+            data.applyLink?data.applyLink:'N/A'
+        ])
+        
     });
 
     scraper.on(events.scraper.error, (err) => {
         console.error(err);
     });
-
+    
     scraper.on(events.scraper.end, () => {
+        stringify(scraped_data, { header: true, columns: columns }, (err, output) => {
+            if (err) throw err;
+            fs.writeFile(`result${timestamp}.csv`, output, (err) => {
+              if (err) throw err;
+              console.log('data is saved.');
+            });
+          });
         console.log('All done!');
     });
 
@@ -79,27 +112,14 @@ const writeStream = fs.createWriteStream('result.csv');
         // Run queries serially
         scraper.run([
             {
-                query: "DevOps",
-                options: {
-                    locations: ["United Kingdom"], // This will be merged with the global options => ["United States", "Europe"]
-                    filters: {
-                        type: [typeFilter.FULL_TIME, typeFilter.CONTRACT]
-                    },
-                }
-            },
-            {
-                query: "devops",
-                options: {
-                    limit: 101, // This will override global option limit (33)
-                }
+                query: "Platform Engineer"
             },
         ], { // Global options for this run, will be merged individually with each query options (if any)
             locations: ["United Kingdom"],
             optimize: true,
-            limit: 100,
+            limit: 50,
         }),
     ]);
-
     // Close browser
-    // await scraper.close();
+    await scraper.close();
 })();
